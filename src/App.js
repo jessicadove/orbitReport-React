7c72e54763305b9875ede08abce8b58120c9f2bb
Jessica <jlknobeloch@gmail.com>
2023-07-19 16:43:49 -0500

    added useState hook, state variable, and corresponding imports

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
+import { useState } from 'react';
+import satData from "./components/satData";
 import Buttons from "./components/Buttons";
 import Table from "./components/Table";
+import Banner from "./components/Banner";
 
 
 function App() {
+  const [sat, setSat] = useState(satData);
+
   return (
     <div>
+      <Banner />
       <Buttons />
       <Table />
     </div>
